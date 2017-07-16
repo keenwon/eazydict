d716019ae5ccb00f0f4e73f929b1f5513b96c510
keenwon <semanwmj@gmail.com>
2017-07-16 14:41:51 +0800

    支持短语查询

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const debug = require('./lib/debug');
 const plugins = require('./lib/plugins');
 const uxCli = require('./ux/cli');
 
-function main(word, options) {
-  debug('word: %s', word);
+function main(...argus) {
+  let words = argus.slice(0, -1).join(' ');
+  let options = argus.slice(-1);
+
+  debug('words: %s', words);
   debug('options: %O', options);
 
   Promise
     .all(plugins.map(plugin => {
-      return plugin(word);
+      return plugin(words);
     }))
     .then(data => {
       let output = uxCli(data);
