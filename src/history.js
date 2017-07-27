c3d876bb560629c50a1074aa9b8634f7f28afa69
keenwon <semanwmj@gmail.com>
2017-07-28 00:44:40 +0800

    历史记录loading

--- src/history.js
+++ src/history.js
@@ -6,11 +6,18 @@ const unicons = require('unicons');
 const lookup = require('./lookup');
 const historyService = require('./service/history');
 const historyCli = require('./cli/history');
+const {
+  loadStart,
+  loadSuccess,
+  loadFail
+} = require('./cli/loader');
 
 /**
  * 历史记录
  */
 function history() {
+  loadStart();
+
   return co(function* () {
     let data = yield historyService.getHistory(10);
 
@@ -27,10 +34,12 @@ function history() {
       return `${index < 10 ? ' ' + index : index}. ${words}`;
     });
 
+    loadSuccess('历史记录查询成功 \n');
     let answer = yield historyCli(list);
 
     return lookup(answer.history, null);
   }).catch(err => {
+    loadFail();
     console.error(err);
   });
 }
