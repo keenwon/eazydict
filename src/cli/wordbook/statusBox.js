be224a796a4ed32f2ae50563f7bcee2ee1c6baf5
keenwon <semanwmj@gmail.com>
2017-07-29 23:44:08 +0800

    添加生词本删除提示

--- src/cli/wordbook/statusBox.js
+++ src/cli/wordbook/statusBox.js
@@ -8,7 +8,7 @@ const blessed = require('blessed');
 function createStatusBox(count) {
   return blessed.text({
     // eslint-disable-next-line max-len
-    content: `  共 ${count} 条 | 退出: Esc、Ctrl-C、q {|} {cyan-fg}{bold} Made With Heart by Keenwon{/bold}  `,
+    content: `  共 ${count} 条 | 退出: Esc、Ctrl-C、q; 删除: d-d {|} {cyan-fg}{bold} Made With Heart by Keenwon{/bold}  `,
     valign: 'middle',
     top: '95%',
     height: '5%',
