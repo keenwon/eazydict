a80fa5996c02ecb64ed7b5e9c13400742e792706
keenwon <semanwmj@gmail.com>
2017-08-05 00:52:18 +0800

    添加cli help

--- src/install.js
+++ src/install.js
@@ -1,19 +1,21 @@
 'use strict';
 
 const npmi = require('npmi');
+const chalk = require('chalk');
 
 function install(pluginName) {
   const options = {
     name: pluginName,
-    forceInstall: false
+    forceInstall: true
   };
 
   npmi(options, function (err, result) {
     if (err) {
-      return console.error(err);
+      return console.error(err.stack);
     }
 
-    console.log(`${options.name} installed successfully`);
+    let message = chalk.green(`${options.name} installed successfully`);
+    console.log(`\n${message}\n`);
   });
 }
 
