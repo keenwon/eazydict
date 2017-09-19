88faaf7f0c59b0b2e7312270e91a3e99db22c455
keenwon <semanwmj@gmail.com>
2017-09-19 23:00:10 +0800

    status边框设置为纯白，保证终端有透明度时也能看到

--- src/cli/status.js
+++ src/cli/status.js
@@ -4,7 +4,10 @@ const Table = require('cli-table2');
 
 module.exports = function (items) {
   let table = new Table({
-    colWidths: [20, 20]
+    colWidths: [20, 20],
+    style: {
+      border: ['white']
+    }
   });
 
   table.push(...items);
